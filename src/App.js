a57f7f8f50802f1755c842d29af66753649ead75
ollinok <62589655+ollinok@users.noreply.github.com>
2022-05-24 22:26:07 +0300

    state variables set

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import ColorSpace from './components/ColorSpace';
 import CheckButtonSpace from './components/CheckButtonSpace';
 import AnswerTitleSpace from './components/AnswerTitleSpace';
 import HelpMenu from './components/HelpMenu';
-
+import { useState } from 'react';
 
 function App() {
+  const [currentColor, setCurrentColor] = useState('red');
+  const [currentRow, setCurrentRow] = useState(10);
+  const [answer, setAnswer] = useState([]);
+  const [guess, setGuess] = useState([]);
+  const [cpuMarkers, setCpuMarkers] = useState([]);
 
   return (
     <>
